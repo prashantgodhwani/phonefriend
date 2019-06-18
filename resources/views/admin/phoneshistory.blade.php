8e87b5044f198e2a68cd329175ecdb70c2512ba1
prashant <prashant.godhwani@hotmail.com>
2019-06-18 23:54:21 +0530

    changes

--- resources/views/admin/phoneshistory.blade.php
+++ resources/views/admin/phoneshistory.blade.php
@@ -40,6 +40,30 @@
                                     </div>
                                 </div>
                             </div>
+                            <div class="row">
+                                <div class="col-xs-6 col-sm-4">
+                                    <div class="card-box widget-flat border-custom bg-custom text-white">
+                                        <i class="fi-tag"></i>
+                                        <h3 class="m-b-10">{{$orders[0]->ccdcSuccess}}</h3>
+                                        <p class="text-uppercase m-b-5 font-13 font-600">CCDC Success</p>
+                                    </div>
+                                </div>
+                                <div class="col-xs-6 col-sm-4">
+                                    <div class="card-box bg-primary widget-flat border-primary text-white">
+                                        <i class="fi-archive"></i>
+                                        <h3 class="m-b-10">{{$codAttempted[0]->ccdcUP}}</h3>
+                                        <p class="text-uppercase m-b-5 font-13 font-600">CCDC Under Process</p>
+                                    </div>
+                                </div>
+                                <div class="col-xs-6 col-sm-4">
+                                    <div class="card-box widget-flat border-success bg-success text-white">
+                                        <i class="fi-help"></i>
+                                        <h3 class="m-b-10">{{$codSuccess[0]->ccdcAborted}}</h3>
+                                        <p class="text-uppercase m-b-5 font-13 font-600">CCDC Aborted</p>
+                                    </div>
+                                </div>
+
+                            </div>
                         </div>
 
 
