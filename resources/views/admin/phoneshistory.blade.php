eedfe86d50fa9325cbd25d66fb54913398d6a006
prashant <prashant.godhwani@hotmail.com>
2019-06-19 00:48:39 +0530

    Changes

--- resources/views/admin/phoneshistory.blade.php
+++ resources/views/admin/phoneshistory.blade.php
@@ -35,27 +35,34 @@
                                 <div class="col-xs-6 col-sm-3">
                                     <div class="card-box bg-danger widget-flat border-danger text-white">
                                         <i class="fi-server"></i>
-                                        <h3 class="m-b-10">{{($ccdcAttempted->isEmpty()) ? '0' :  $ccdcAttempted[0]->sales}}</h3>
-                                        <p class="text-uppercase m-b-5 font-13 font-600">CCDC Attempted</p>
+                                        <h3 class="m-b-10">{{($codCancelled->isEmpty()) ? '0' :  $codCancelled[0]->sales}}</h3>
+                                        <p class="text-uppercase m-b-5 font-13 font-600">COD Cancelled</p>
                                     </div>
                                 </div>
                             </div>
                             <div class="row">
-                                <div class="col-xs-6 col-sm-4">
+                                <div class="col-xs-6 col-sm-3">
+                                    <div class="card-box widget-flat border-custom bg-custom text-white"  style="background-color: #4f88ef !important; border-color: #4f88ef !important;">
+                                        <i class="fi-tag"></i>
+                                        <h3 class="m-b-10">{{($ccdcAttempted->isEmpty()) ? '0' :  $ccdcAttempted[0]->sales}}</h3>
+                                        <p class="text-uppercase m-b-5 font-13 font-600">CCDC Success</p>
+                                    </div>
+                                </div>
+                                <div class="col-xs-6 col-sm-3">
                                     <div class="card-box widget-flat border-custom bg-custom text-white"  style="background-color: #8828a7 !important; border-color: #8828a7 !important;">
                                         <i class="fi-tag"></i>
                                         <h3 class="m-b-10">{{($ccdcSuccess->isEmpty()) ? '0' : $ccdcSuccess[0]->sales}}</h3>
                                         <p class="text-uppercase m-b-5 font-13 font-600">CCDC Success</p>
                                     </div>
                                 </div>
-                                <div class="col-xs-6 col-sm-4">
+                                <div class="col-xs-6 col-sm-3">
                                     <div class="card-box bg-primary widget-flat border-primary text-white"  style="background-color: #2bceb0 !important; border-color: #2bceb0 !important;">
                                         <i class="fi-archive"></i>
                                         <h3 class="m-b-10">{{($ccdcUP->isEmpty()) ? '0' : $ccdcUP[0]->sales}}</h3>
                                         <p class="text-uppercase m-b-5 font-13 font-600">CCDC Under Process</p>
                                     </div>
                                 </div>
-                                <div class="col-xs-6 col-sm-4">
+                                <div class="col-xs-6 col-sm-3">
                                     <div class="card-box widget-flat border-success bg-success text-white"  style="background-color: #e28817 !important; border-color: #e28817 !important;">
                                         <i class="fi-help"></i>
                                         <h3 class="m-b-10">{{($ccdcAborted->isEmpty()) ? '0' : $ccdcAborted[0]->sales}}</h3>
