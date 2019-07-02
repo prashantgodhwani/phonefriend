2e37c17c40fe84d621fa68aa9afe5c363b000459
prashant <prashant.godhwani@hotmail.com>
2019-07-02 23:51:16 +0530

    Orientation UI

--- resources/views/layouts/other.blade.php
+++ resources/views/layouts/other.blade.php
@@ -1,25 +1,54 @@
 <!DOCTYPE html>
 <html lang="en-US" itemscope="itemscope" itemtype="http://schema.org/WebPage">
-<meta http-equiv="content-type" content="text/html;charset=utf-8" />
-<head><!-- Global site tag (gtag.js) - Google Ads: 801041161 --> <script async src="https://www.googletagmanager.com/gtag/js?id=AW-801041161"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-801041161'); </script>
-    <!-- Global site tag (gtag.js) - Google Ads: 801041161 --> <script async src="https://www.googletagmanager.com/gtag/js?id=AW-801041161"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-801041161'); </script>
+<meta http-equiv="content-type" content="text/html;charset=utf-8"/>
+<head><!-- Global site tag (gtag.js) - Google Ads: 801041161 -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-801041161"></script>
+    <script> window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+        gtag('config', 'AW-801041161'); </script>
+    <!-- Global site tag (gtag.js) - Google Ads: 801041161 -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-801041161"></script>
+    <script> window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+        gtag('config', 'AW-801041161'); </script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     @yield('meta')
-    <meta name="keywords" content="cheap second hand phones for sale, buy refurbished iphone in india, box opened mobile phones, certified pre owned mobile phones, tablets & laptops online in india with huge discounts, smartphone store in noida, second hand iphones for sale, old samsung phones for sale, second hand samsung phones for sale, refurbished oneplus phones, second hand and used oppo mobile phone, second hand vivo mobile phone, mi second hand phone, motorola second hand phone, nokia second hand phones, blackberry second hand phone, gionee second hand phone, lenovo second hand phone">
+    <meta name="keywords"
+          content="cheap second hand phones for sale, buy refurbished iphone in india, box opened mobile phones, certified pre owned mobile phones, tablets & laptops online in india with huge discounts, smartphone store in noida, second hand iphones for sale, old samsung phones for sale, second hand samsung phones for sale, refurbished oneplus phones, second hand and used oppo mobile phone, second hand vivo mobile phone, mi second hand phone, motorola second hand phone, nokia second hand phones, blackberry second hand phone, gionee second hand phone, lenovo second hand phone">
     <link rel="canonical" href="https://www.phonefriend.in/">
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Facebook Pixel Code -->
     <script>
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window,document,'script',
+        !function (f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function () {
+                n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window, document, 'script',
             'https://connect.facebook.net/en_US/fbevents.js');
         fbq('init', '2250243695224974');
         fbq('track', 'PageView');
@@ -30,47 +59,52 @@
 &noscript=1"/>
     </noscript>
     <!-- End Facebook Pixel Code -->
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/bootstrap.min.css')}}" media="all" />
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/font-awesome.min.css')}}" media="all" />
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/animate.min.css')}}" media="all" />
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/font-electro.css')}}" media="all" />
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/owl-carousel.css')}}" media="all" />
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/style.css?v=1.3')}}" media="all" />
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/colors/green.css')}}" media="all" />
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendor-prefixes.css')}}" media="all" />
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/responsiveslides.css')}}" media="all" />
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/bootstrap.min.css')}}" media="all"/>
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/font-awesome.min.css')}}" media="all"/>
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/animate.min.css')}}" media="all"/>
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/font-electro.css')}}" media="all"/>
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/owl-carousel.css')}}" media="all"/>
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/style.css?v=1.3')}}" media="all"/>
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/colors/green.css')}}" media="all"/>
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendor-prefixes.css')}}" media="all"/>
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/responsiveslides.css')}}" media="all"/>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
-    <link href="{{asset('assets/css/jquery.steps.css')}}" rel="stylesheet" type="text/css" media="all" />
+    <link href="{{asset('assets/css/jquery.steps.css')}}" rel="stylesheet" type="text/css" media="all"/>
 
-    <link href="{{asset('assets/css/c588c3c0468f4633c99de6e16289c863.css')}}" rel="stylesheet" type="text/css" media="all" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" />
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,700italic,800,800italic,600italic,400italic,300italic' rel='stylesheet' type='text/css'>
-    <link href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap-social/5.1.1/bootstrap-social.css' rel='stylesheet' type='text/css'>
+    <link href="{{asset('assets/css/c588c3c0468f4633c99de6e16289c863.css')}}" rel="stylesheet" type="text/css"
+          media="all"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css"/>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,700italic,800,800italic,600italic,400italic,300italic'
+          rel='stylesheet' type='text/css'>
+    <link href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap-social/5.1.1/bootstrap-social.css' rel='stylesheet'
+          type='text/css'>
 
     <link rel="shortcut icon" href="{{asset('assets/images/fav-icon.png')}}">
     <style>
-        .fa-stack[data-count]:after{
-            position:absolute;
-            right:0%;
-            top:1%;
+        .fa-stack[data-count]:after {
+            position: absolute;
+            right: 0%;
+            top: 1%;
             content: attr(data-count);
-            font-size:30%;
-            padding:.6em;
-            border-radius:999px;
-            line-height:.75em;
+            font-size: 30%;
+            padding: .6em;
+            border-radius: 999px;
+            line-height: .75em;
             color: white;
-            background:rgb(232, 34, 45);;
-            text-align:center;
-            min-width:2em;
-            font-weight:bold;
+            background: rgb(232, 34, 45);;
+            text-align: center;
+            min-width: 2em;
+            font-weight: bold;
         }
+
         .lazyload,
         .lazyloading {
             opacity: 0.5;
             transition: opacity 300ms;
         }
+
         .lazyloaded {
             opacity: 1;
             transition: opacity 300ms;
@@ -80,7 +114,11 @@
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-121571191-1"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
         gtag('js', new Date());
 
         gtag('config', 'UA-121571191-1');
@@ -89,10 +127,20 @@
 </head>
 
 <body class="home-v2">
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MGB9VQV"
-                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MGB9VQV"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
 <div class="lockLandscape" style="display: none">
-    <div class="mvW8vq"><div class="_1U-Jli"></div><div class="FqIVIZ">Please rotate your device</div><div class="_1wxP6t">We don't support landscape mode yet. Please go back to portrait mode for the best experience</div></div>
+    <div class="mvW8vq container container-table">
+        <div class="row vertical-center-row">
+            <div class="_1U-Jli container"></div>
+            <div class="FqIVIZ">Please rotate your device</div>
+            <div class="_1wxP6t">We don't support landscape mode yet. Please go back to portrait mode for the best
+                experience
+            </div>
+        </div>
+    </div>
 </div>
 <div id="page" class="hfeed site">
     <a class="skip-link screen-reader-text" href="#site-navigation">Skip to navigation</a>
@@ -123,22 +171,22 @@
     var index = client.initIndex('phones');
     //initialize autocomplete on search input (ID selector must match)
     autocomplete('#aa-search-input',
-        { hint: false }, {
+        {hint: false}, {
             source: autocomplete.sources.hits(index, {hitsPerPage: 5, filters: 'units_rem!=0 AND sold!=2'}),
             //value to be displayed in input control after user's suggestion selection
             displayKey: 'name',
             //hash of templates used when rendering dataset
             templates: {
                 //'suggestion' templating function used to render a single suggestion
-                suggestion: function(suggestion) {
-                    var str=suggestion.dp;
+                suggestion: function (suggestion) {
+                    var str = suggestion.dp;
 
                     return '<span><a href="' +
                         suggestion.url +
                         '">' +
-                        suggestion._highlightResult.company.value+" "+suggestion._highlightResult.model.value + '</span><span>' +
+                        suggestion._highlightResult.company.value + " " + suggestion._highlightResult.model.value + '</span><span>' +
                         '<img src="../storage/' +
-                        str.replace("public","")+
+                        str.replace("public", "") +
                         '" style="width: 60px">' + '</a></span>';
                 }
             }
@@ -146,7 +194,7 @@
 </script>
 <script type="text/javascript" src="https://kenwheeler.github.io/slick/slick/slick.js"></script>
 <script type="text/javascript">
-    $(document).ready(function(){
+    $(document).ready(function () {
         $('.notice-module').show();
         $('.notice-module').slick({
             slidesToShow: 1,
@@ -154,16 +202,16 @@
             autoplay: true,
             autoplaySpeed: 3000,
             arrows: false,
-            vertical : true
+            vertical: true
         });
     });
 </script>
 <script>
-    window.addEventListener("orientationchange", function() {
-        if(window.orientation === 90 || window.orientation === -90){
+    window.addEventListener("orientationchange", function () {
+        if (window.orientation === 90 || window.orientation === -90) {
             $('.lockLandscape').css("display", "block");
 
-        }else{
+        } else {
             $('.lockLandscape').css("display", "none");
         }
     }, false);
