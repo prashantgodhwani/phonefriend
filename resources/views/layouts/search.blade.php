e7cbde617b883d0a47e5a4a732926453b4d246c3
prashant <prashant.godhwani@hotmail.com>
2019-07-07 02:36:06 +0530

    jquery

--- resources/views/layouts/search.blade.php
+++ resources/views/layouts/search.blade.php
@@ -101,8 +101,9 @@
 </div><!-- #page -->
 
 
-<script type="text/javascript" src="assets/js/jquery.min.js"></script>
-
+<script
+        src="https://code.jquery.com/jquery-2.2.4.js"
+></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.2.0/min/dropzone.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
 @yield('scripts')
