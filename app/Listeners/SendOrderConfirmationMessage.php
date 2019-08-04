0d9aa4f613ea6e69570c84df061613cfed4fa48a
prashant <prashant.godhwani@hotmail.com>
2019-08-04 18:04:08 +0530

    CHanged password SMSIndiaHub

--- app/Listeners/SendOrderConfirmationMessage.php
+++ app/Listeners/SendOrderConfirmationMessage.php
@@ -46,17 +46,8 @@ class SendOrderConfirmationMessage
             $str .= ucwords(Phone::find($order->orderdevices[0]->phone_id)->data->company) . ' ' . Phone::find($order->orderdevices[0]->phone_id)->data->model;
         }
         $response = Curl::to('http://103.8.127.46/vendorsms/pushsms.aspx')
-            ->withData( array( 'user' => 'pra.godh', 'password' => 'R@dhasoami30', 'msisdn' => $order->deliver_phone,'sid' => 'PHNFRN' , 'msg' => 'Order Received: We have received your order for '.$str.' with order id '.$order->order_id.' amounting to Rs.'.number_format($order->amount).'. The order shall be delivered within 5-6 business days. You can manage your order at https://goo.gl/Hkvifi .','fl'=>'0','gwid'=>'2') )
+            ->withData( array( 'user' => 'pra.godh', 'password' => 'Sandy@2019', 'msisdn' => $order->deliver_phone,'sid' => 'PHNFRN' , 'msg' => 'Order Received: We have received your order for '.$str.' with order id '.$order->order_id.' amounting to Rs.'.number_format($order->amount).'. The order shall be delivered within 5-6 business days. You can manage your order at https://goo.gl/Hkvifi .','fl'=>'0','gwid'=>'2') )
             ->asJson()
             ->get();
-        foreach($order->orderdevices as $device){
-            $phone = User::find(Phone::find($device->phone_id)->user_id)->phone;
-            $date = Carbon::now();
-            $date = $date->addDays(2)->toFormattedDateString();
-            $responsem = Curl::to('http://103.8.127.46/vendorsms/pushsms.aspx')
-                ->withData( array( 'user' => 'pra.godh', 'password' => 'R@dhasoami30', 'msisdn' => $phone,'sid' => 'PHNFRN' , 'msg' => 'Dear Merchant, Your product with ID #TT-0A'.$device->phone_id.' has been sold. The pickup of your order is scheduled on '.$date.'. Please carefully pack the product keep it ready with all the accessories.','fl'=>'0','gwid'=>'2') )
-                ->asJson()
-                ->get();
-        }
     }
 }
